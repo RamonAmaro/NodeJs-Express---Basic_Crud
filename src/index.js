a24b9ba5f18498de9c1ebe3a5228e2353d78e581
RamonAmaro <ramonamaro.dev@gmail.com>
2022-01-04 17:24:42 -0300

    finisih basic crud

--- src/index.js
+++ src/index.js
@@ -152,4 +152,24 @@ app.get("/accounts", (request, response) => {
   return response.status(200).json(customers);
 });
 
+app.delete(
+  "/account",
+  MiddlewareVerifyIfExistsAccountCPF,
+  (request, response) => {
+    const { customer } = request;
+
+    customers.splice(customer, 1);
+
+    return response.status(200).json(customers);
+  }
+);
+
+app.get("/balance", MiddlewareVerifyIfExistsAccountCPF, (request, response) => {
+  const { customer } = request;
+
+  const balance = getBalance(customer.statement);
+
+  return response.status(200).json({ balance });
+});
+
 app.listen(3333);
